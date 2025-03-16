7b6af5da2c888d80379fe485af20a03d08e143a7
Richie Cahill <Richie@tmmworkshop.com>
2025-03-16 14:18:02 -0400

    removed run_migrations_offline

--- alembic/env.py
+++ alembic/env.py
@@ -37,30 +37,6 @@ def dynamic_schema(filename: str, _options: dict[Any, Any]) -> None:
     Path(filename).write_text(dynamic_schema_file)
 
 
-def run_migrations_offline(url: URL) -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-# This is part of the Alembic specification
 def include_name(
     name: str | None,
     type_: Literal["schema", "table", "column", "index", "unique_constraint", "foreign_key_constraint"],
@@ -107,7 +83,4 @@ def run_migrations_online(url: URL) -> None:
 url = get_url("MXR")
 
 
-if context.is_offline_mode():
-    run_migrations_offline(url)
-else:
-    run_migrations_online(url)
+run_migrations_online(url)
